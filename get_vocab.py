bf24ebc04a7090b354f92d920529af3627cc2f99
graehl <graehl@gmail.com>
2017-07-18 18:00:47 -0700

    NFC for get_vocab.py

--- get_vocab.py
+++ get_vocab.py
@@ -2,14 +2,27 @@
 from __future__ import print_function
 import sys
 from collections import Counter
+from unicodedata import normalize
 
+# python 2/3 compatibility
+if sys.version_info < (3, 0):
+    sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
+    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+    sys.stdin = codecs.getreader('UTF-8')(sys.stdin)
+else:
+    sys.stderr = codecs.getwriter('UTF-8')(sys.stderr.buffer)
+    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout.buffer)
+    sys.stdin = codecs.getreader('UTF-8')(sys.stdin.buffer)
+
+# K makes roman numeral I -> ascii I. C leaves as is.
+form = sys.argv[1] if len(sys.argv) == 1 else 'NFC'
 
 c = Counter()
 
 for line in sys.stdin:
     for word in line.split():
         if len(word):
-            c[word] += 1
+            c[unicodedata.normalize(form, word)] += 1
 
 for key,f in c.most_common():
     print(key+" "+ str(f))
